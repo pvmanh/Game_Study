59d4f2c33980c62e97ed532e1a67d508b263fde7
TranKhaiMinh <45234059+TranKhaiMinh@users.noreply.github.com>
2020-12-05 15:40:56 +0700

    type document

---
+++ Study_Game/Assets/Script/Type_Document/Scut.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scut : MonoBehaviour
+{
+    //public Text iText;
+    public string sChuoi_Input;
+    string A;
+    string Final;
+    // Start is called before the first frame update
+    void Start()
+    {
+        A = sChuoi_Input;
+        catchuoi();
+
+        //iText.text = Final;
+    }
+    void catchuoi()
+    {   
+        for(int i = 0; i < sChuoi_Input.Length; i++)
+        {
+            if(A.IndexOf(" ") >= 0)
+            {
+                string B = A.Remove(A.IndexOf(" "));
+                A = A.Substring(A.IndexOf(" "));
+
+                
+
+                if(B.Length != 0)
+                {
+                    Final += B + " ";
+                }
+                else if(B.Length == 0)
+                {
+                    A = A.Remove(0,1);
+                }
+            }
+            else
+            {
+                Final += A;
+                i = sChuoi_Input.Length;
+                Debug.Log(Final.Length);
+            }
+        }
+
+        
+    }
+}
--- Study_Game/Assets/Script/Type_Document/WordDoc.cs
+++ Study_Game/Assets/Script/Type_Document/WordDoc.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 using TMPro;
 public class WordDoc : MonoBehaviour
 {
-
+ 
+    string A;
+    string Final;
+    string stringgoc;
     public List<string> WordList = new List<string> { };
     public string[] wordListcb;
     public GameObject GO;
@@ -13,15 +16,39 @@ public class WordDoc : MonoBehaviour
     public TMP_InputField IPWord;
     private string currentWord = string.Empty;
     private string reWord = string.Empty;
-    private string tudung = string.Empty;
-    private string tusai = string.Empty;
+    private string txtdung = string.Empty;
+    private string txtsai = string.Empty;
+    private int k = 0;
+    public TimeModel timeData;
+    //time
+    //public float timeStart = 0;
+   // public Text textTime;
+    //score
+    public Text textAccurary;
+    public Text textSpeed;
+    public TextMeshProUGUI ttAccurary;
+    public TextMeshProUGUI ttSpeed;
+    public TextMeshProUGUI ttTime;
+    public GameObject total;
+    private float accurary = 0;
+    private float tudung = 0;
+    private float tusai = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        timeData.timegget = timeData.txt_time.text;
         wordListcb = catList(WordList, wordListcb, 0);
+
         getList();
-       // Debug.Log(wordListcb);
-       
+        catchuoi();
+        currentWord = Final;
+        currentWord += " ";
+        WordDocOP.text = Final;
+        
+         Debug.Log(Final);
+        Debug.Log(stringgoc);
 
     }
 
@@ -33,58 +60,143 @@ public class WordDoc : MonoBehaviour
     }
     public void getList()
     {
-        currentWord = wordListcb[0];
-        WordDocOP.text = wordListcb[0];
-        Debug.Log(WordDocOP.text);    
+        
+        stringgoc = wordListcb[0];
+        A = stringgoc;
+        //WordDocOP.text = wordListcb[0];
+        //currentWord += " ";
+       /* for (int i = 0; i < currentWord.Length; i++)
+        {
+           // if(currentWord.Substring(currentWord.IndexOf(" ")))
+            //{
+                Debug.Log(currentWord.Remove(currentWord.IndexOf(" ")));
+            //}
+        }
+            Debug.Log(WordDocOP.text);   */ 
+    }
+    void catchuoi()
+    {
+        for (int i = 0; i < stringgoc.Length; i++)
+        {
+            if (A.IndexOf(" ") >= 0)
+            {
+                string B = A.Remove(A.IndexOf(" "));
+                A = A.Substring(A.IndexOf(" "));
+
+
+
+                if (B.Length != 0)
+                {
+                    Final += B + " ";
+                }
+                else if (B.Length == 0)
+                {
+                    A = A.Remove(0, 1);
+                }
+            }
+            else
+            {
+                Final += A;
+                i = stringgoc.Length;
+                Debug.Log(Final.Length);
+            }
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-         getWord();
+           // timeStart += Time.deltaTime;
+        if (tudung != 0 || tusai != 0)
+        {
+            accurary = (tudung / (tusai + tudung)) * 100;
+        }
+            //textTime.text = Mathf.RoundToInt(timeStart).ToString();
+            textAccurary.text = Mathf.RoundToInt(accurary).ToString();
+            textSpeed.text = Mathf.RoundToInt(tudung).ToString();
+          
+        getWord();
         colorWord();
+    }
+    void FixedUpdate()
+    {
+        Timer.TimeClock(timeData);
     }
 
     public void colorWord()
     {
         WordDocOP.text = reWord +currentWord;
     }
+    void test()
+    {
+
+    }
     public void getWord()
     {
-        
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            
-            for (int i =0; i < currentWord.Length; i++)
+           // k++;
+            if ( IPWord.text!=" "&& IPWord.text != "  " &&IPWord.text != "   " && IPWord.text != "    " && IPWord.text != "     ")
             {
-      
-                if (currentWord.Substring(i, 1) == " ")
+                for (int i = 0; i < currentWord.Length; i++)
                 {
-                    tusai = null;
-                    tudung = null;
-                    if (IPWord.text == currentWord.Substring(0,i+1))
-                    {
-                        //Debug.Log(currentWord.Substring(0, i));
-                        tudung = "<color=green>" + currentWord.Substring(0, i+1) + "</color>";
-                        currentWord = currentWord.Remove(0, i+1);
-                        reWord += tudung;
-                        Debug.Log(tudung);
-                    }
-                   else if(IPWord.text != currentWord.Substring(0, i+1))
-                    {
-                        
-                        tusai = "<color=red>" +  currentWord.Substring(0, i+1) + "</color>";
-                       currentWord = currentWord.Remove(0, i+1);
-                        reWord += tusai;
-                        Debug.Log(tusai);
 
+                    if (currentWord.Substring(i, 1) == " ")
+                    {
+                        txtsai = null;
+                        txtdung = null;
+                        if (IPWord.text == currentWord.Substring(0, i + 1))
+                        {
+                            Debug.Log(currentWord.Substring(0, i));
+                            txtdung = "<color=green>" + currentWord.Substring(0, i + 1) + "</color>";
+                            currentWord = currentWord.Remove(0, i + 1);
+                            reWord += txtdung;
+                            tudung++;
+                            Debug.Log(txtdung);
+                        }
+                        else if (IPWord.text != currentWord.Substring(0, i + 1))
+                        {
+
+                            txtsai = "<color=red>" + currentWord.Substring(0, i + 1) + "</color>";
+                            currentWord = currentWord.Remove(0, i + 1);
+                            reWord += txtsai;
+                            tusai++;
+                             Debug.Log(txtsai);
+
+                        }
+                        i = currentWord.Length;
+                        //if (k >= 1)
+                      //  {
+                            IPWord.text = null;
+                            //Debug.Log(tudung);
+                            //Debug.Log(tusai);
+                       // }
                     }
-                    i = currentWord.Length;
-                    IPWord.text = null;
-                    //Debug.Log(tudung);
-                    //Debug.Log(tusai);
                 }
             }
+
+            if (IPWord.text == "     ")
+            {
+                IPWord.text = null;
+            }
+            Debug.Log(currentWord+"1");
+            if (currentWord.Length == 0)
+            {
+                total.SetActive(true);
+                WordDocOP.gameObject.SetActive(false);
+                IPWord.gameObject.SetActive(false);
+                Time.timeScale = 0;
+                ttAccurary.text = Mathf.RoundToInt(accurary).ToString();
+                ttSpeed.text = Mathf.RoundToInt(tudung).ToString();
+                ttTime.text = timeData.txt_time.text;
+            }
         }
+       /* else
+        {
+            k = 0;
+        }*/
     }
 }
