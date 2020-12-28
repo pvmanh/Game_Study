374140bc36a2ec68d146d0ab641a06e84b9dc3b2
TranKhaiMinh <45234059+TranKhaiMinh@users.noreply.github.com>
2020-12-28 19:46:11 +0700

    fix

---
+++ Study_Game/Assets/Script/paint/testscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FreeDraw;
+
+public class testscript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //-5 va 5, -3.7 va 3.7 tu do trong play
+        if(mouse_pos.x >= -5 && mouse_pos.x <= 5f && mouse_pos.y >= -3.7f && mouse_pos.y <= 3.7f)
+        {
+            transform.position = new Vector3(mouse_pos.x, mouse_pos.y, -1.02f);
+        }
+    }
+}
